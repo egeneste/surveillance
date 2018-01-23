863c80af59c35d032dc538640ee35907e3103daa
eleazar <elzr.geneste@gmail.com>
2018-01-22 23:36:50 -0400

    temp

--- interface/urls.py
+++ interface/urls.py
@@ -6,8 +6,8 @@ from .views import *
 
 urlpatterns = [
     url(r'^$', index),
-    url(r'^region-uno/$', regionUno),
-    url(r'^temperatura/$', tableTemperatura)
+    url(r'region-uno/$', regionUno),
+    url(r'temperatura/$', tableTemperatura)
 ]
 
 if settings.DEBUG :
